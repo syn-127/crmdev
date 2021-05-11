5e368a5fa63f286693267b0276f3b1242f0bfefb
Winterseye <admin@savagenutrition.co.uk>
2021-05-11 23:52:29 +0100

    Started coding API , added to api.php and added changes to Prospect.php , prospects.php and ProspectsController.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Prospects;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/* Get Request for the API to show all current prospects / leads on the CRM */
+
+Route::get('/prospects', [Prospects\ProspectsController::class, 'index']);
+
+/* Post request to create a prospect on the database via API */
+
+Route::post('/prospects', [Prospects\ProspectsController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
